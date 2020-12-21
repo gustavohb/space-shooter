e84a5a758bef272f471750490b72fc8925a6e914
Gustavo Barrionuevo <gustavohb@gmail.com>
2020-12-21 14:44:32 -0300

    Basic pool system

---
+++ SpaceShooter_Project/Assets/Scripts/Singleton/SingletonMonoBehavior.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+    static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    _instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    GameObject _gameObject;
+    Transform _transform;
+
+    protected GameObject _GameObject
+    {
+        get
+        {
+            if (_gameObject == null)
+            {
+                _gameObject = this.gameObject;
+            }
+            return _gameObject;
+        }
+    }
+
+    protected Transform _Transform
+    {
+        get
+        {
+            if (_transform == null)
+            {
+                _transform = transform;
+            }
+            return _transform;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (this != Instance)
+        {
+            GameObject obj = gameObject;
+            Destroy(this);
+            Destroy(obj);
+            return;
+        }
+    }
+}
