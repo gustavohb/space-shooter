712ac6705b4960ada2af4c7a3ecf5106b8617e71
Gustavo H. Barrionuevo <gustavohb@gmail.com>
2020-12-22 10:10:43 -0300

    Basic player shooting

---
+++ SpaceShooter_Project/Assets/Scripts/Spaceships/Player/PlayerShooting.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : ExtendedCustomMonoBehavior
+{
+    [SerializeField] private PlayerWeapon[] _weapons;
+
+    [SerializeField] private int _weaponIndex = 0;
+
+    public int WeaponIndex => _weaponIndex;
+
+    private PlayerWeapon _currentWeapon;
+
+    private Transform _currentTarget;
+
+    [SerializeField] private Color _bulletColor = Color.magenta;
+
+    public string targetTag = "Enemy";
+
+    [SerializeField] private float _findClosestTargetDelay = 1.5f;
+    private float _findClosestTargetTimer = 0.0f;
+
+    [SerializeField] private Rect _findTargetArea;
+
+    private void Awake()
+    {
+        SetWeapon(0);
+    }
+
+    private void Update()
+    {
+        HandleShootBlasterProjectile();
+    }
+
+    public void SetWeapon(int setValue)
+    {
+        if (_weaponIndex >= 0)
+        {
+            if (_currentWeapon)
+            {
+                Destroy(_currentWeapon.gameObject);
+            }
+
+            if (setValue >= 0 && setValue < _weapons.Length)
+            {
+                _currentWeapon = Instantiate(_weapons[setValue], transform.position, Quaternion.identity);
+                _currentWeapon.transform.parent = transform;
+                _currentWeapon.targetTag = targetTag;
+            }
+        }
+    }
+
+    public void FindClosestTarget()
+    {
+        _currentTarget = null;
+
+        foreach (GameObject possibleTarget in GameObject.FindGameObjectsWithTag(targetTag))
+        {
+            if (_currentTarget == null)
+            {
+                if (possibleTarget.transform.position.x < _findTargetArea.width && possibleTarget.transform.position.x > _findTargetArea.x && possibleTarget.transform.position.y < _findTargetArea.height && possibleTarget.transform.position.y > _findTargetArea.y)
+                {
+                    _currentTarget = possibleTarget.transform;
+                }
+            }
+            else if (possibleTarget.transform.position.x < _findTargetArea.width && possibleTarget.transform.position.x > _findTargetArea.x && possibleTarget.transform.position.y < _findTargetArea.height && possibleTarget.transform.position.y > _findTargetArea.y)
+            {
+                if (Vector3.Distance(transform.position, possibleTarget.transform.position) < Vector3.Distance(transform.position, _currentTarget.position))
+                {
+                    _currentTarget = possibleTarget.transform;
+                }
+            }
+        }
+
+        if (_currentTarget != null && _currentWeapon != null)
+        {
+            _currentWeapon.SetTarget(_currentTarget);
+        }
+
+        if (_currentTarget == null && _currentWeapon != null)
+        {
+            _currentWeapon?.GetComponent<PlayerWeapon>().StopShooting();
+        }
+        else
+        {
+            _currentWeapon?.GetComponent<PlayerWeapon>().StartShooting();
+        }
+
+        if (_currentTarget && _currentWeapon)
+        {
+            _currentWeapon.GetComponent<PlayerWeapon>().SetTarget(_currentTarget);
+        }
+    }
+
+    private void HandleShootBlasterProjectile()
+    {
+
+        _findClosestTargetTimer += Time.deltaTime;
+
+        if (_findClosestTargetTimer > _findClosestTargetDelay || _currentTarget == null)
+        {
+            FindClosestTarget();
+            _findClosestTargetTimer = 0.0f;
+        }
+    }
+
+    public void ChangeWeapon(int changeValue)
+    {
+        if (_weaponIndex + changeValue < _weapons.Length)
+        {
+            _weaponIndex += changeValue;
+
+            SetWeapon(_weaponIndex);
+        }
+    }
+}
---
+++ SpaceShooter_Project/Assets/Scripts/Spaceships/Player/PlayerWeapon.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+
+public class PlayerWeapon : BaseShot
+{
+
+    [SerializeField] private Transform[] _muzzles;
+
+    [SerializeField] private float _burstDelay = 0f;
+
+    [SerializeField] private int _burstShots = 1;
+
+    [SerializeField] private float _shotRate = 0.5f;
+
+    [SerializeField] private float betweenDelay = 0.1f;
+
+    public string targetTag = "Enemy";
+
+    [HideInInspector]
+    public bool isShooting { get; private set; } = false;
+
+    private Transform _currentTarget;
+
+    private float _burstDelayCount = 0f;
+
+    private int _burstShotsCount = 0;
+
+    private float _shotRateCount = 0f;
+
+    private float angle;
+
+    public void Update()
+    {
+        HandleShooting();
+    }
+
+    private void HandleShooting()
+    {
+        if (_currentTarget == null)
+        {
+            return;
+        }
+
+        Vector3 newEulerAngles = transform.eulerAngles;
+
+        newEulerAngles.z = Mathf.Atan2(_currentTarget.position.y - transform.position.y, _currentTarget.position.x - transform.position.x) * (180 / Mathf.PI);
+
+        transform.eulerAngles = newEulerAngles;
+
+        _burstDelayCount += Time.deltaTime;
+
+        if (_burstDelayCount >= _burstDelay)
+        {
+            if (_burstShotsCount < _burstShots)
+            {
+                _shotRateCount += Time.deltaTime;
+
+                if (_shotRateCount >= _shotRate)
+                {
+                    _burstShotsCount++;
+
+                    foreach (Transform muzzleTransform in _muzzles)
+                    {
+                        Bullet bullet = GetBullet(muzzleTransform.position, muzzleTransform.rotation);
+                        bullet.targetTag = targetTag;
+
+                        angle = Util.GetAngleFromTwoPosition(transform, _currentTarget);
+
+                        ShotBullet(bullet, bulletSpeed, angle);
+
+                        AutoReleaseBulletGameObject(bullet.gameObject);
+                    }
+
+                    _shotRateCount = 0;
+                }
+            }
+            else
+            {
+                _burstShotsCount = 0;
+                _burstDelayCount = 0;
+            }
+        }
+    }
+
+    public void StartShooting()
+    {
+        if (isShooting || _currentTarget == null)
+        {
+            return;
+        }
+        isShooting = true;
+        _burstShotsCount = 0;
+    }
+
+    public void StopShooting()
+    {
+        isShooting = false;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _currentTarget = target;
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (_muzzles == null || _muzzles.Length == 0)
+        {
+            return;
+        }
+
+        foreach (Transform muzzleTranform in _muzzles)
+        {
+            Gizmos.color = Color.red;
+
+            Vector3 direction = muzzleTranform.TransformDirection(transform.right) * 5;
+
+            Gizmos.DrawRay(muzzleTranform.position, direction);
+        }
+    }
+
+    public override void Shot()
+    {
+        
+    }
+
+    public override void StopShot()
+    {
+        
+    }
+}
+
