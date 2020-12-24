99900294659f041f527aa1754c9945d6e0f013f3
Gustavo H. Barrionuevo <gustavohb@gmail.com>
2020-12-24 17:24:03 -0300

    Fix time bug

--- SpaceShooter_Project/Assets/Scripts/Time/GameTime.cs
+++ SpaceShooter_Project/Assets/Scripts/Time/GameTime.cs
@@ -3,5 +3,5 @@
 public static class GameTime
 {
     public static bool isPaused = false;
-    public static float deltaTime { get { return isPaused ? 0 : GameTime.deltaTime; } }
+    public static float deltaTime { get { return isPaused ? 0 : Time.deltaTime; } }
 }
